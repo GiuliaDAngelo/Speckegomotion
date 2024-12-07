68117ddbd462ed567a4e3a03b2b5f5977ff31335
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2024-12-07 11:16:37 +0100

    refactoring main egomotion + correcting network

--- egomotionlayer_functions.py
+++ egomotionlayer_functions.py
@@ -3,7 +3,6 @@
 from egomotionlayer_functions import *
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt
-from configmain import *
 import pickle
 import numpy as np
 import h5py
@@ -53,13 +52,13 @@ def load_eventsnpy(polarity, dur_video, FPS, filePathOrName,tsFLAG,time_wnd_fram
     # find out maximum x and y
 
     ### values here are in milliseonds the max ts should be the duration of the video 4 sec
-    max_x = rec['x'].max().astype(int)
-    max_y = rec['y'].max().astype(int)
+    max_x = rec['x'].max().astype(int) + 1
+    max_y = rec['y'].max().astype(int) + 1
     max_ts = rec['t'].max()
     # use single polarity
     rec['p'][rec['p'] == False] = True
     rec['p'] = polarity
-    sensor_size = (max_x + 1, max_y + 1, 1)
+    sensor_size = (max_x, max_y, 1)
     # print(f"sensor size is {sensor_size}")
     # We have to convert the raw events into frames so that we can feed those to our network
     # We use a library called tonic for that https://tonic.readthedocs.io/en/latest/ as well as torchvision
@@ -69,40 +68,6 @@ def load_eventsnpy(polarity, dur_video, FPS, filePathOrName,tsFLAG,time_wnd_fram
     ])
     frames = transforms(rec)
     return frames,max_y, max_x
-
-
-def egokernel():
-    # create kernel Gaussian distribution
-    gauss_kernel_center = gaussian_kernel(size_krn_center, sigma_center)
-    gauss_kernel_center = gauss_kernel_center/gauss_kernel_center.max()
-
-    gauss_kernel_surround = gaussian_kernel(size_krn_surround, sigma_surround)
-    gauss_kernel_surround = gauss_kernel_surround/gauss_kernel_surround.max()
-
-    # plot_kernel(gauss_kernel_center,gauss_kernel_center.size(0))
-    # plot_kernel(gauss_kernel_surround,gauss_kernel_surround.size(0))
-
-    filter =  gauss_kernel_center - gauss_kernel_surround
-    # plot_kernel(filter, filter.size(0))
-    filter = filter.unsqueeze(0)
-    return filter
-
-def OMS():
-    # create kernel Gaussian distribution
-    center = gaussian_kernel(size_krn_center, sigma_center).unsqueeze(0)
-    surround = gaussian_kernel(size_krn_surround, sigma_surround).unsqueeze(0)
-    return center, surround
-
-
-def net_def(filter, tau_mem, num_pyr, size_krn, device, stride):
-    # define our single layer network and load the filters
-    net = nn.Sequential(
-        nn.Conv2d(1, num_pyr, (size_krn,size_krn), stride=stride, bias=False),
-        sl.LIF(tau_mem),
-    )
-    net[0].weight.data = filter.unsqueeze(1).to(device)
-    net[1].v_mem = net[1].tau_mem * net[1].v_mem.to(device)
-    return net
 
 
 def egomotion(window, net_center, net_surround, device, max_y, max_x,threshold):
@@ -124,6 +89,43 @@ def egomotion(window, net_center, net_surround, device, max_y, max_x,threshold):
     return OMS, indexes
 
 
+def npy_data(filePathOrName, tsFLAG):
+    recording = np.load(filePathOrName)
+    if tsFLAG:
+        recording[:, 3] *= 1e3  # convert time from seconds to milliseconds
+    rec = rf.unstructured_to_structured(recording,
+                                        dtype=np.dtype(
+                                            [('x', np.int16), ('y', np.int16), ('p', bool), ('t', int)]))
+    return rec
+
+
+def createfld(respath, namefld):
+    # Check if the folder exists, and create it if it doesn't
+    if not os.path.exists(respath+namefld):
+        os.makedirs(respath+namefld)
+        print('Folder created')
+    else:
+        print('Folder already exists')
+
+
+
+def savekernel(kernel, size, name):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = torch.linspace(0, size, size)
+    y = torch.linspace(0, size, size)
+    x, y = torch.meshgrid(x, y)
+    ax.plot_surface(x.numpy(), y.numpy(), kernel[0].numpy(), cmap='jet')
+    plt.show()
+    plt.savefig(name+'.png')
+
+
+def OMSkernels(size_krn_center, sigma_center, size_krn_surround, sigma_surround):
+    # create kernel Gaussian distribution
+    center = gaussian_kernel(size_krn_center, sigma_center).unsqueeze(0)
+    surround = gaussian_kernel(size_krn_surround, sigma_surround).unsqueeze(0)
+    return center, surround
+
 def gaussian_kernel(size, sigma):
     # Create a grid of (x, y) coordinates using PyTorch
     x = torch.linspace(-size // 2, size // 2, size)
@@ -134,214 +136,58 @@ def gaussian_kernel(size, sigma):
     # Normalize the kernel so that the values are between 0 and 1
     kernel = (kernel - kernel.min()) / (kernel.max() - kernel.min())
     return kernel
-#
-# def plot_kernel(kernel,size):
-#     #plot kernel 3D
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-#     x = torch.linspace(-size // 2, size // 2, size)
-#     y = torch.linspace(-size // 2, size // 2, size)
-#     x, y = torch.meshgrid(x, y)
-#     ax.plot_surface(x.numpy(), y.numpy(), kernel.numpy(), cmap='jet')
-#     plt.show()
-#
-# def h5load_data(filename):
-#     with h5py.File(filename, "r") as f:
-#         # Print all root level object names (aka keys)
-#         # these can be group or dataset names
-#         print("Keys: %s" % f.keys())
-#         # get first object name/key; may or may NOT be a group
-#         a_group_key = list(f.keys())[0]
-#
-#         # get the object type for a_group_key: usually group or dataset
-#         print(type(f[a_group_key]))
-#
-#         # If a_group_key is a group name,
-#         # this gets the object names in the group and returns as a list
-#         data = list(f[a_group_key])
-#
-#         # If a_group_key is a dataset name,
-#         # this gets the dataset values and returns as a list
-#         data = list(f[a_group_key])
-#         # preferred methods to get dataset values:
-#         ds_obj = f[a_group_key]  # returns as a h5py dataset object
-#         ds_arr = f[a_group_key][()]  # returns as a numpy array
-#         print(ds_arr)
-#     return data, ds_arr
-#
-#
-# class WinnerTakesAll(nn.Module):
-#     def __init__(self, k=1):
-#         super(WinnerTakesAll, self).__init__()
-#         self.k = k
-#
-#     def forward(self, x):
-#         # Flatten the input except for the batch dimension
-#         flat_x = x.view(x.size(0), -1)
-#         # Get the top-k values and their indices
-#         topk_vals, topk_indices = torch.topk(flat_x, self.k, dim=1)
-#         # Create a mask of the same shape as flat_x
-#         mask = torch.zeros_like(flat_x)
-#         # Set the top-k values in the mask to 1
-#         mask.scatter_(1, topk_indices, 1)
-#         # Reshape the mask to the original input shape
-#         mask = mask.view_as(x)
-#         # Apply the mask to the input
-#         return x * mask
-#
-# def net_def(filter, tau_mem, num_pyr):
-#     # define our single layer network and load the filters
-#     device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-#     net = nn.Sequential(
-#         nn.Conv2d(1, num_pyr, (size_krn,size_krn), stride=1, bias=False),
-#         sl.LIF(tau_mem),
-#     )
-#     net[0].weight.data = filter.unsqueeze(1).to(device)
-#     net[1].v_mem = net[1].tau_mem * net[1].v_mem.to(device)
-#     return net
-#
-#
-#
-def npy_data(filePathOrName, tsFLAG):
-    recording = np.load(filePathOrName)
-    if tsFLAG:
-        recording[:, 3] *= 1e3  # convert time from seconds to milliseconds
-    rec = rf.unstructured_to_structured(recording,
-                                        dtype=np.dtype(
-                                            [('x', np.int16), ('y', np.int16), ('p', bool), ('t', int)]))
-    return rec
-#
-# def load_krn(path):
-#     filters = []
-#     filter = np.load(path)
-#     filter = torch.tensor(np.stack(filter).astype(np.float32))
-#     filter = filter.unsqueeze(0)
-#     return filter
-#
-#
-# def pyr_res(num_pyr,frames):
-#     res = []
-#     for pyr in range(1, num_pyr + 1):
-#         res.append((int((frames[0][0].shape[0]) / pyr), int((frames[0][0].shape[1]) / pyr)))
-#     return res
-#
-# def running_stats(
-#     x: torch.Tensor,
-#     mean: torch.Tensor,
-#     var: torch.Tensor,
-#     alpha: float = 0.1,
-#     eps: float = 1e-9,
-# ):
-#     """
-#     Exponential running statistics.
-#     """
-#
-#     diff = x - mean
-#     inc = alpha * diff
-#     mean += inc
-#     var = (1.0 - alpha) * (var + diff * inc)
-#     sd = torch.sqrt(var + eps)
-#
-#     return (mean, var, sd)
-#
-#
-# def plot_runningmean(numframes,time_wnd_frames,meanegomap,respath,title):
-#     print(meanegomap)
-#     time = np.arange(0, (numframes-1)*time_wnd_frames* 1e-3, time_wnd_frames* 1e-3)
-#     plt.plot(time, meanegomap)
-#     plt.xlabel('time [ms]')# plotting by columns
-#     plt.ylabel('running mean - network activity')
-#     # plt.show()
-#     plt.savefig(respath+'/'+title, dpi=300)  # Save as PNG with 300 dpi
-#     return
-#
-#
-#
-#
-#
-# def run(filter, frames, max_x, max_y,time_wnd_frames):
-#
-#     # Define motion parameters
-#     tau_mem = time_wnd_frames* 10**-3 #tau_mem in milliseconds
-#     #Initialize the network with the loaded filter
-#     net = net_def(filter, tau_mem, num_pyr)
-#     cnt=0
-#     device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-#     for frame in frames:
-#         print(str(cnt) + " frame out of " + str(frames.shape[0]))
-#         frame = frame.to(device, dtype=net[0].weight.dtype)
-#         egomap = net(frame)
-#         #resize egomap to the original size
-#         egomap = torch.nn.functional.interpolate(egomap.unsqueeze(0), size=(max_y+1, max_x+1), mode='bilinear', align_corners=False).squeeze(0)
-#         #frame, egomap between 0 and 255
-#         frame = (frame - frame.min()) / (frame.max() - frame.min()) * 255
-#         egomap = (egomap - egomap.min()) / (egomap.max() - egomap.min()) * 255
-#         #values under a threashold are set to 0
-#         egomap[egomap < threshold] = 0
-#         # create suppression map
-#         suppression = torch.zeros((1, max_y + 1, max_x + 1), device=device)
-#         #where egomap is over the threashold suppression max = frame
-#         suppression[egomap >= threshold] = frame[egomap >= threshold]
-#
-#         # Show the egomap
-#         if show_egomap:
-#             # plot the frame and overlap the max point of the saliency map with a red dot
-#             plt.clf()
-#             #subplot ahowing the frame and the egomap
-#             plt.subplot(1, 3, 1)
-#             plt.imshow(frame.squeeze(0).cpu().detach().numpy(), cmap='gray')
-#             plt.colorbar(shrink=0.3)
-#             plt.title('Frame')
-#
-#             plt.subplot(1, 3, 2)
-#             plt.imshow(egomap.squeeze(0).cpu().detach().numpy(), cmap='jet')
-#             plt.colorbar(shrink=0.3)
-#             plt.title('Egomap Map')
-#
-#             #plot suppression map
-#             plt.subplot(1, 3, 3)
-#             plt.imshow(suppression.squeeze(0).cpu().detach().numpy(), cmap='gray')
-#             plt.colorbar(shrink=0.3)
-#             plt.title('Suppression Map')
-#
-#             plt.draw()
-#             plt.pause(0.001)
-#         if save_res:
-#             # save the plot in a video
-#             plt.savefig(respath + 'egomaps/egomap' + str(cnt) + '.png')
-#         cnt+=1
-#
-#
-#
-#
-# def load_eventsh5(polarity, time_wnd_frames, ds_arr):
-#     # structured the data
-#     ds_arr[:, 0] -= int(ds_arr[0][0])  # starting from the first timestep
-#     rec_data = rf.unstructured_to_structured(ds_arr,
-#                                              dtype=np.dtype(
-#                                                  [('t', int), ('x', np.int16), ('y', np.int16), ('p', bool)]))
-#
-#     # get the sensor size
-#     max_x = rec_data['x'].max().astype(int)
-#     max_y = rec_data['y'].max().astype(int)
-#     max_ts = rec_data['t'].max()
-#     # use single polarity
-#     rec_data['p'][rec_data['p'] == False] = True
-#     rec_data['p'] = polarity
-#     sensor_size = (max_x + 1, max_y + 1, 1)
-#     # print(f"sensor size is {sensor_size}")
-#     # convert events into frames
-#     transforms = torchvision.transforms.Compose([
-#         tonic.transforms.ToFrame(sensor_size=sensor_size, time_window=time_wnd_frames),
-#         torch.tensor,
-#     ])
-#     frames = transforms(rec_data)
-#     return frames, max_y, max_x
-#
-def createfld(respath, namefld):
-    # Check if the folder exists, and create it if it doesn't
-    if not os.path.exists(respath+namefld):
-        os.makedirs(respath+namefld)
+
+def net_def(filter, tau_mem, num_pyr, size_krn, device, stride):
+    # define our single layer network and load the filters
+    net = nn.Sequential(
+        nn.Conv2d(1, num_pyr, (size_krn,size_krn), stride=stride, bias=False),
+        sl.LIF(tau_mem),
+    )
+    net[0].weight.data = filter.unsqueeze(1).to(device)
+    net[1].v_mem = net[1].tau_mem * net[1].v_mem.to(device)
+    return net
+
+
+def egomotion(window, net_center, net_surround, device, max_y, max_x,threshold):
+    window = window.unsqueeze(0).float().to(device)
+    center = net_center(window)
+    surround = net_surround(window)
+    center = torch.nn.functional.interpolate(center.unsqueeze(0), size=(max_y, max_x), mode='bilinear',
+                                             align_corners=False).squeeze(0)
+    surround = torch.nn.functional.interpolate(surround.unsqueeze(0), size=(max_y, max_x), mode='bilinear',
+                                             align_corners=False).squeeze(0)
+
+    events = center - surround
+    events = 1 - (events - events.min())/(events.max() - events.min())
+    indexes = events >= threshold
+
+    if indexes.any():
+        OMS = torch.zeros_like(events)
+        OMS[indexes] = 255
+    else:
+        OMS = torch.zeros_like(events)
+
+    # center = (center - center.min()) / (center.max() - center.min())
+    # surround = (surround - surround.min()) / (surround.max() - surround.min())
+    # center = center * 255
+    # surround = surround * 255
+    # events = events * 255
+    # fig, axs = plt.subplots(1, 4, figsize=(15, 10))
+    # axs[0].cla()
+    # axs[1].cla()
+    # axs[2].cla()
+    # axs[3].cla()
+    # axs[0].imshow(center[0].cpu().detach().numpy(), cmap='gray', vmin=0, vmax=255)
+    # axs[1].imshow(surround[0].cpu().detach().numpy(), cmap='gray', vmin=0, vmax=255)
+    # axs[2].imshow(events[0].cpu().detach().numpy(), cmap='gray', vmin=0, vmax=255)
+    # axs[3].imshow(OMS[0].cpu().detach().numpy(), cmap='gray', vmin=0, vmax=255)
+    # plt.draw()
+    # plt.pause(0.001)
+    return OMS, indexes
+
+def mkdirfold(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
         print('Folder created')
     else:
         print('Folder already exists')
--- mainegomotion.py
+++ mainegomotion.py
@@ -1,7 +1,6 @@
 '''
 Model developed by:
 Giulia D'Angelo
-Alexander Hadjiivanov
 
 Summary:
 This script loads a Gaussian filter, initializes a spiking neural network, and processes event-based data from a video file.
@@ -14,12 +13,51 @@ Steps:
 3. Run the spiking neural network on the data.
 4. Save the generated frames and create videos from the resulting maps.
 '''
-from configmain import *
+
 from egomotionlayer_functions import *
 
 import matplotlib
 matplotlib.use('TkAgg')
 
+# Parameters kernel
+size_krn_center = 8  # Size of the kernel (NxN) (all half ) - 8
+sigma_center = 2  # Sigma for the first Gaussian - 1
+size_krn_surround = 8  # Size of the kernel (NxN) - 8
+sigma_surround = 4  # Sigma for the first Gaussian - 4
+
+# Parameters network
+threshold = 0.80
+num_pyr = 1
+
+# Parameters events
+polarity = True
+FPSvideo = 60.0  # Frames per second
+dur_video = 4  # Duration of video in seconds
+tsFLAG = False  # Flag to convert timestamps to microseconds
+
+# Flags
+show_egomap = True
+save_res = False
+
+# Paths
+# exp =  'objego'
+# exp =  'ego1'
+# exp =  'ego3'
+# exp =  'ego4'
+# exp =  'ego5'
+# exp =  'ego8'
+# exp =  'onlyobj'
+# exp = '1'
+# exp = '02'
+# exp = '4'
+exp = 'invertedspeeds'
+
+
+respath = 'results/'+exp+'/'
+evpath = '/Users/giuliadangelo/workspace/code/IEBCS/data/video/egomotionstimuli/'+exp+'/'+exp+'.npy'
+
+
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 
 if __name__ == '__main__':
 
@@ -27,61 +65,51 @@ if __name__ == '__main__':
     if save_res:
         create_results_folders(respath)
 
-    # loading egomotion kernel
-    filter_egomotion = egokernel()
+    # Load events
+    time_wnd_frames = 1000 #1Khz as IEBCS
+    [frames, max_y, max_x] = load_eventsnpy(polarity, dur_video, FPSvideo, evpath, tsFLAG, time_wnd_frames)
+    len_fr = len(frames)
+    time_wnd_frames = dur_video/len_fr
+    tau_mem = time_wnd_frames
 
-    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-    stride= 1
-    # Initialize the network with the loaded filter
-    netegomotion = net_def(filter_egomotion,tau_mem, num_pyr, filter_egomotion.size(1), device, stride)
+    #define network
+    center, surround = OMSkernels(size_krn_center, sigma_center, size_krn_surround, sigma_surround)
+    ss = 1
+    sc = ss + sigma_surround - sigma_center
+    net_center = net_def(center, tau_mem, num_pyr, size_krn_center, device, sc)
+    net_surround = net_def(surround, tau_mem, num_pyr, size_krn_surround, device, ss)
 
-    characterisationFLAG = True
-
-    if characterisationFLAG:
-        # Load event-based data from a specified .npy file.
-        time_wnd_frames = 400
-        [frames, max_y, max_x] = load_eventsnpy(polarity, dur_video, FPSvideo, filePathOrName, tsFLAG, time_wnd_frames)
-        # run(filter, frames, max_x, max_y,time_wnd_frames)
-        len_fr = len(frames)
-        time_wnd_frames = dur_video/len_fr
-
-        # Define motion parameters
-        cnt=0
-        spikes = [[] for _ in range((max_x+1)*(max_y+1))]
-        time = 0
-        for frame in frames.numpy():
-            time = time+time_wnd_frames
-            print(str(cnt) + " frame out of " + str(frames.shape[0]))
-            frame = (frame[0] > 0).astype(int)
-            egomap, indexes = egomotion(frame, netegomotion, 1, device, (max_y + 1), (max_x + 1))
-            #coordinates where indexes True
-            indtrue = np.argwhere(indexes[0].cpu() == True)
-            for i in range(len(indtrue[0])):
-                x = indtrue[1][i]
-                y = indtrue[0][i]
-                spikes[x+(y*max_x)].append(time)
-            # Show the egomap
-            if show_egomap:
-                # plot the frame and overlap the max point of the saliency map with a red dot
-                plt.clf()
-
-                #plot suppression map
-                plt.imshow(egomap.squeeze(0), cmap='gray', vmin=0, vmax=255)
-                plt.colorbar(shrink=0.3)
-                # plt.title('Suppression Map')
-
-                plt.draw()
-                plt.pause(0.001)
-            if save_res:
-                # save the plot in a video
-                plt.savefig(respath + 'egomaps/egomap' + str(cnt) + '.png')
-            cnt+=1
-
-        with open(name_exp+'.pkl', 'wb') as f:
-            pickle.dump(spikes, f)
-
-        # Save the results as videos
+    cnt=0
+    spikes = [[] for _ in range((max_x+1)*(max_y+1))]
+    time = 0
+    for frame in frames:
+        time = time+time_wnd_frames
+        print(str(cnt) + " frame out of " + str(frames.shape[0]))
+        OMS, indexes = egomotion(frame[0], net_center, net_surround, device, max_y, max_x, threshold)
+        #coordinates where indexes True
+        indtrue = np.argwhere(indexes[0].cpu() == True)
+        for i in range(len(indtrue[0])):
+            x = indtrue[1][i]
+            y = indtrue[0][i]
+            spikes[x+(y*max_x)].append(time)
+        # Show the egomap
+        if show_egomap:
+            plt.clf()
+            plt.imshow(OMS[0].cpu().numpy(), cmap='gray', vmin=0, vmax=255)
+            plt.colorbar(shrink=0.3)
+            # plt.title('Suppression Map')
+            plt.draw()
+            plt.pause(0.001)
         if save_res:
-            create_video_from_frames(respath+'egomaps/',  respath+respath.split('/')[1]+'egomap.mp4', fps=FPSvideo)
+            # save the plot in a video
+            plt.savefig(respath + 'egomaps/egomap' + str(cnt) + '.png')
+        cnt+=1
 
-        print('end')
+    with open(respath+'spikes.pkl', 'wb') as f:
+        pickle.dump(spikes, f)
+
+    # Save the results as videos
+    if save_res:
+        create_video_from_frames(respath+'egomaps/',  respath+respath.split('/')[1]+'egomap.mp4', fps=FPSvideo)
+
+    print('end')
