af0b408aa14490f77910923c22e089828340d522
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2024-12-31 17:18:05 +0100

    removing PTU from code

--- SpeckOMSattention.py
+++ SpeckOMSattention.py
@@ -20,18 +20,18 @@ update_interval = 0.001 #0.02 #seconds
 last_update_time = time.time()
 
 
-# Parameters kernel
-size_krn_center = 8  # Size of the kernel (NxN)
-sigma_center = 1  # Sigma for the first Gaussian
-size_krn_surround = 8  # Size of the kernel (NxN)
-sigma_surround = 4  # Sigma for the first Gaussian
+# Parameters OMS
+size_krn_center = 8  # Size of the kernel (NxN) (all half ) - 8
+sigma_center = 1  # Sigma for the first Gaussian - 1
+size_krn_surround = 8  # Size of the kernel (NxN) - 8
+sigma_surround = 4  # Sigma for the first Gaussian - 4
 
+# Parameters network
+threshold = 0.80
 num_pyr = 1
-tau_mem = 0.01
-threshold = 200
 
 
-# Visual attention paramters
+# Parameters attention
 size = 10  # Size of the kernel
 r0 = 4  # Radius shift from the center
 rho = 0.1  # Scale coefficient to control arc length
@@ -41,12 +41,12 @@ offsetpxs = 0 #size / 2
 offset = (offsetpxs, offsetpxs)
 fltr_resize_perc = [2, 2]
 num_pyr = 3
-
-# Create Von Mises (VM) filters with specified parameters
 # The angles are generated in radians, ranging from 0 to 2π in steps of π/4
 thetas = np.arange(0, 2 * np.pi, np.pi / 4)
 
 
+
+tau_mem = 0.1
 if __name__ == "__main__":
 
     device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     net_surround = net_def(surround, tau_mem, num_pyr, size_krn_surround, device, ss)
 
     ##### attention #####
-    VMkernels = create_vm_filters(thetas, size, rho, r0, thick, offset, fltr_resize_perc)
+    VMkernels = create_vm_filters(thetas, size, rho, r0, thick, offset)
     # plot_filters(filters_attention, thetas)
     netattention = net_def(VMkernels, tau_mem, num_pyr, size, device, 1)
 
@@ -89,47 +89,18 @@ if __name__ == "__main__":
         with events_lock:
             if current_time - last_update_time > update_interval:
                 if numevs[0] > 0:
-                    egomap, indexes = egomotion(window, netegomotion, numevs, device)
+                    egomap, indexes = egomotion(window, net_center, net_surround, device, max_y, max_x, threshold)
                     salmap, salmap_coords = run_attention(egomap, netattention, device)
                     if showstats:
                         attention_maps.append(salmap)
                         salmaps_coordinates.append(salmap_coords)
                         windows.append(egomap[0])
-                    cv2.imshow('Events', window)
-                    cv2.imshow('Events after Suppression', egomap[0])
+                    cv2.imshow('Events map', window)
+                    cv2.imshow('OMS map', egomap[0])
                     cv2.circle(salmap, (salmap_coords[1], salmap_coords[0]), 5, (255, 255, 255), -1)
                     cv2.imshow('Saliency Map', cv2.applyColorMap(cv2.convertScaleAbs(salmap), cv2.COLORMAP_JET))
                     cv2.waitKey(1)
                     window.fill(0)
-                    if pantiltunit:
-                        # are the location of maximum attention value, values are between -1 and 1
-                        cmd = run_controller(
-                                np.array([salmap_coords[1]/(resolution[1]), salmap_coords[0]/(resolution[1])]),
-                                np.array([0.5, 0.5]),
-                                k_pan=np.array([2.,0.,0.]),
-                                k_tilt=np.array([2.,0.,0.]),
-                            )
-
-                        # sending commands to the pantilt unit; format the command (assuming the device uses "PP<angle>" and "TP<angle>")
-                        # rescale cmd to pan_range & tilt_range
-                        pan_angle = int((cmd[0] * (pan_range[1] - pan_range[0]) / 2) + (pan_range[1] + pan_range[0]) / 2)
-                        tilt_angle = int(
-                            (cmd[1] * (tilt_range[1] - tilt_range[0]) / 2) + (tilt_range[1] + tilt_range[0]) / 2)
-
-                        with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
-                            pan_command = f'PP{pan_angle}\n'
-                            tilt_command = f'TP{tilt_angle}\n'
-
-                            # Send the pan and tilt commands
-                            send_command(ser, f'PU\n')
-                            send_command(ser, pan_command)
-                            send_command(ser, f'TU\n')
-                            send_command(ser, tilt_command)
-                            send_command(ser, f'A\n')
-                            response = ser.readline().decode('utf-8').strip()
-                        if response:
-                            print(f"Response from device: {response}")
-                            pass
                     if showstats:
                         #print number of events
                         # print('Number of events: ' + str(numevs[0]))
