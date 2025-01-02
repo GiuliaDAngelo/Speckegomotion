c6a6e8b9196ba889064074e1bfe57744395b0558
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2025-01-02 14:02:01 +0100

    refactoring + checkout

--- characteriseattention.py
+++ characteriseattention.py
@@ -1,99 +1,30 @@
-# Import the function 'importIitYarp' from the 'bimvee' library to load event-based camera data
-import matplotlib
-import torch
 import cv2
-import os
-import yaml
-from functions.data_helpers import load_yarp_events
+import torch
+import numpy as np
+from functions.data_helpers import load_yarp_events, create_results_folders
 from functions.OMS_helpers import *
-from functions.attention_helpers import *
+from functions.attention_helpers import AttentionModule
+import matplotlib
 matplotlib.use('TkAgg')
 
 
 
-##########################
-####### Parameters #######
-##########################
 
-# Define the dimensions of the event-based camera (304 pixels width by 240 pixels height)
-max_x = 304
-max_y = 240
-resolution = (max_y, max_x)
-
-# Parameters OMS
-size_krn_center = 8  # Size of the kernel (NxN) (all half ) - 8
-sigma_center = 1  # Sigma for the first Gaussian - 1
-size_krn_surround = 8  # Size of the kernel (NxN) - 8
-sigma_surround = 4  # Sigma for the first Gaussian - 4
-threshold = 0.50
-tau_memOMS = 0.1  # Time constant for the membrane potential
-
-#Parameters attention
-VM_radius=10
-VM_radius_group=15
-num_ori=4
-b_inh=1.5
-g_inh=1.0
-w_sum=0.5
-vm_w=.1
-vm_w2=.4
-vm_w_group=.2
-vm_w2_group=.4
-random_init=False
-lif_tau=0.1
-
-
-
-if __name__ == '__main__':
-    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-
-    #####################
-    ##### load data #####
-    #####################
-    camera_events = 'right'
-    codec = '20bit'
-    # codec = '24bit'
-
-    # filePathOrName = '/Users/giuliadangelo/workspace/data/DATASETs/attention-multiobjects/'
-    # filePathOrName = '/Users/giuliadangelo/workspace/data/DATASETs/IROS_attention/calibration/obj/ATIS/'
-    filePathOrName = '/Users/giuliadangelo/workspace/data/DATASETs/IROS_attention/calib_circles/calibration_circles/ATIS/'
-    [e_x,e_y,e_ts,e_pol] = load_yarp_events(filePathOrName,codec,camera_events)
-
-    #####################
-    ##### egomotion #####
-    #####################
-    center, surround = OMSkernels(size_krn_center, sigma_center, size_krn_surround, sigma_surround)
-    ss = 1
-    sc = 1 #ss + sigma_surround - sigma_center
-    net_center = net_def(center, tau_memOMS, 1, 1, size_krn_center, device, sc)
-    net_surround = net_def(surround, tau_memOMS, 1, 1, size_krn_surround, device, ss)
-
-    #####################
-    ##### attention #####
-    #####################
-    # Initialize the attention module with parameters from the YAML file
-    am = AttentionModule(VM_radius=VM_radius,
-                         VM_radius_group=VM_radius_group,
-                         num_ori=num_ori,
-                         b_inh=b_inh,
-                         g_inh=g_inh,
-                         w_sum=w_sum,
-                         vm_w=vm_w,
-                         vm_w2=vm_w2,
-                         vm_w_group=vm_w_group,
-                         vm_w2_group=vm_w2_group,
-                         random_init=random_init,
-                         lif_tau=lif_tau)
-
-    # Set up parameters for processing events
-    time_window = 400
-    window_period = time_window
-    image_w = 304
-    image_h = 240
+def process_events(events, attention_module, device, time_window=300):
+    """Process events and display saliency maps."""
+    e_x, e_y, e_ts, e_pol = events
+    image_h, image_w = config.MAX_Y, config.MAX_X
     window_pos = np.zeros((image_h, image_w), dtype=np.float32)
     window_neg = np.zeros((image_h, image_w), dtype=np.float32)
+    window_period = time_window
 
-    # Process events and generate saliency maps
+    # Create results folders
+    if config.RES_PATH:
+        create_results_folders(config.RES_PATH, '/events')
+        create_results_folders(config.RES_PATH, '/oms')
+        create_results_folders(config.RES_PATH, '/attention')
+
+    cnt = 0
     for x, y, ts, pol in zip(e_x, e_y, e_ts, e_pol):
         if ts <= window_period:
             if pol == 1:
@@ -101,14 +32,88 @@ if __name__ == '__main__':
             else:
                 window_neg[y][x] = 1.0
         else:
+            #OMS
+            window_combined = window_pos + window_neg
+            window_combined[window_combined != 0] = 1.0
+            window_OMS = torch.tensor(window_combined, dtype=torch.float32).unsqueeze(0).to(device)
+            OMS, indexes = egomotion(window_OMS, net_center, net_surround, device, config.MAX_Y, config.MAX_X, config.OMS_PARAMS['threshold'])
+            #ATTENTION
             vSlice = np.expand_dims(np.stack((window_pos, window_neg)), 0)
             with torch.no_grad():
-                saliency_map = am(torch.tensor(vSlice,
-                                               dtype=torch.float32,
-                                               requires_grad=False).to(device)).cpu().numpy()
-
-            cv2.imshow('salmap', cv2.applyColorMap((saliency_map * 255).astype(np.uint8), cv2.COLORMAP_JET))
+                saliency_map = attention_module(
+                    torch.tensor(vSlice, dtype=torch.float32).to(device)
+                ).cpu().numpy()
+            #visualisation
+            window_combined = window_combined * 255
+            cv2.imshow('Events map', window_combined)
+            cv2.imshow('OMS map',OMS[0][0].cpu().detach().numpy())
+            cv2.imshow('Saliency map', cv2.applyColorMap(
+                (saliency_map * 255).astype(np.uint8), cv2.COLORMAP_JET))
+            cv2.imwrite(f"{config.RES_PATH}events/Eventmap{cnt}.png", window_combined)
+            cv2.imwrite(f"{config.RES_PATH}oms/OMSmap{cnt}.png", OMS[0].cpu().numpy().squeeze())
+            cv2.imwrite(f"{config.RES_PATH}attention/Saliencymap{cnt}.png", cv2.applyColorMap(
+                (saliency_map * 255).astype(np.uint8), cv2.COLORMAP_JET))
             cv2.waitKey(1)
             window_period += time_window
-            window_pos = np.zeros((image_h, image_w), dtype=np.float32)
-            window_neg = np.zeros((image_h, image_w), dtype=np.float32)
+            window_pos.fill(0)
+            window_neg.fill(0)
+            cnt += 1
+
+
+class Config:
+    # Constants
+    MAX_X, MAX_Y = 304, 240
+    RESOLUTION = (MAX_Y, MAX_X)
+    CAMERA_EVENTS = 'right'
+    CODEC = '20bit'
+
+    EXP = 'no_obj'
+    RES_PATH = 'results/attention/'+EXP+'/'
+    # FILE_PATH = '/Users/giuliadangelo/workspace/data/DATASETs/IROS_attention/calib_circles/calibration_circles/ATIS/'
+    FILE_PATH = '/Users/giuliadangelo/workspace/data/DATASETs/IROS_attention/calibration/no_obj/ATIS/'
+    # FILE_PATH = '/Users/giuliadangelo/workspace/data/DATASETs/IROS_attention/calibration/obj/ATIS/'
+
+
+    # OMS Parameters
+    OMS_PARAMS = {
+        'size_krn_center': 8,
+        'sigma_center': 1,
+        'size_krn_surround': 8,
+        'sigma_surround': 4,
+        'threshold': 0.50,
+        'tau_memOMS': 0.1,
+        'sc':1,
+        'ss':1
+    }
+
+    # Attention Parameters
+    ATTENTION_PARAMS = {
+        'VM_radius': 8, #(R0)
+        'VM_radius_group': 15,
+        'num_ori': 4,
+        'b_inh': 1.5,
+        'g_inh': 1.0,
+        'w_sum': 0.5,
+        'vm_w': 0.2, #(rho)
+        'vm_w2': 0.4,
+        'vm_w_group': 0.2,
+        'vm_w2_group': 0.4,
+        'random_init': False,
+        'lif_tau': 0.1
+    }
+
+
+
+
+
+if __name__ == '__main__':
+    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+    config = Config()
+    # Load data
+    events = load_yarp_events(config.FILE_PATH, config.CODEC, config.CAMERA_EVENTS)
+    # Initialize OMS
+    net_center, net_surround = initialize_oms(device, config.OMS_PARAMS)
+    # Initialize Attention modules
+    net_attention = AttentionModule(**config.ATTENTION_PARAMS)
+    # Process and visualize events
+    process_events(events, net_attention, device)
--- functions/createVMkernel.py
+++ functions/createVMkernel.py
@@ -6,53 +6,100 @@ from mpl_toolkits.mplot3d import Axes3D
 
 matplotlib.use('TkAgg')
 
-# Function to create a Difference of Gaussian kernel in PyTorch
-def difference_of_gaussian(size, sigma1, sigma2):
-    # Create a grid of (x, y) coordinates using PyTorch
-    x = torch.linspace(-size // 2, size // 2, size)
-    y = torch.linspace(-size // 2, size // 2, size)
-    x, y = torch.meshgrid(x, y)
 
-    # Create two Gaussian kernels with different sigmas
-    gaussian1 = (torch.exp(-(x**2 + y**2) / (2 * sigma1**2)))/(np.sqrt(2*np.pi)*sigma1)
+def zero_2pi_tan(x, y):
+    """
+    Compute the angle in radians between the positive x-axis and the point (x, y),
+    ensuring the angle is in the range [0, 2π].
 
-    gaussian2 = (torch.exp(-(x**2 + y**2) / (2 * sigma2**2)))/(np.sqrt(2*np.pi)*sigma2)
+    Args:
+        x (float): x-coordinate of the point.
+        y (float): y-coordinate of the point.
 
-    # Calculate Difference of Gaussian (DoG)
-    dog = gaussian1 - gaussian2
+    Returns:
+        angle (float): Angle in radians, between 0 and 2π.
+    """
+    angle = np.arctan2(y, x) % (2 * np.pi)  # Get the angle in radians and wrap it in the range [0, 2π]
+    return angle
 
-    # Normalize the DoG values between 0 and 1
-    # dog_normalized = (dog - dog.min()) / (dog.max() - dog.min())
-    return dog
+def vm_filter(theta, scale, rho, r0, offset):
+    # theta, scale, rho = 0.1, r0 = 0, thick = 0.5, offset = (0, 0)
+    """Generate a Von Mises filter with r0 shifting and an offset."""
+    height, width = scale, scale
+    vm = np.empty((height, width))
+    offset_x, offset_y = offset
+    for x in range(width):
+        for y in range(height):
+            # Shift X and Y based on r0 and offset
+            X = (x - width / 2) + r0 * np.cos(theta) + offset_x * np.cos(theta)
+            Y = (height / 2 - y) + r0 * np.sin(theta) + offset_y * np.sin(theta)  # Inverted Y for correct orientation
+            r = np.sqrt(X**2 + Y**2 - r0)
+            angle = zero_2pi_tan(-Y, X)
 
-# # Parameters
-# size = 25  # Size of the kernel (NxN)
-# sigma1 = 0.04*size  # Sigma for the first Gaussian
-# sigma2 = 2*size  # Sigma for the second Gaussian
-#
-# # Generate DoG kernel as a tensor
-# dog_kernel = difference_of_gaussian(size, sigma1, sigma2)
-#
-# # Plot 3D surface
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # Create meshgrid for 3D plotting
-# x = torch.linspace(-size // 2, size // 2, size).numpy()
-# y = torch.linspace(-size // 2, size // 2, size).numpy()
-# x, y = np.meshgrid(x, y)
-#
-# # Plot the surface
-# ax.plot_surface(x, y, dog_kernel, cmap='viridis')
-#
-# # Labels and title
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Amplitude')
-# ax.set_title('Difference of Gaussian Kernel (PyTorch)')
-#
-# plt.show()
-# np.save('dog_kernel.npy', dog_kernel)
-#
-#
-# print('end')
+            # Compute the Von Mises filter value
+            # vm[y, x] = np.exp(thick*rho * r0 * np.cos(angle - theta)) / iv(0, r - r0)
+            vm[y, x] = np.exp(rho * r0 * np.cos(angle - theta)) / iv(0, r)
+    return vm
+
+def create_vm_filters(thetas, size, rho, r0, offset):
+    """
+    Create a set of Von Mises filters with different orientations.
+
+    Args:
+        thetas (np.ndarray): Array of angles in radians.
+        size (int): Size of the filter.
+        rho (float): Scale coefficient to control arc length.
+        r0 (int): Radius shift from the center.
+
+    Returns:
+        filters (list): List of Von Mises filters.
+    """
+    filters = []
+    for theta in thetas:
+        filter = vm_filter(theta, size, rho=rho, r0=r0, offset=offset)
+        filters.append(filter)
+    filters = torch.tensor(np.stack(filters).astype(np.float32))
+    return filters
+
+def plot_VMkernels(VMkernels,thetas):
+    num_filters = len(VMkernels)
+    num_rows = (num_filters + (len(VMkernels)//2) - 1) // (len(VMkernels)//2)  # Calculate the number of rows needed
+
+    fig, axes = plt.subplots(num_rows, (len(VMkernels)//2), figsize=(15, 3 * num_rows))
+    axes = axes.flatten()  # Flatten the axes array for easy iteration
+
+    for i, (filter, theta) in enumerate(zip(VMkernels, thetas)):
+        ax = axes[i]
+        ax.imshow(filter.cpu().numpy(), cmap='jet')
+        ax.set_title(f'Theta: {theta:.2f}')
+        ax.axis('off')
+
+    # Hide any remaining empty subplots
+    for j in range(i + 1, len(axes)):
+        axes[j].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # Parameters visual attention
+    sizeVM = 42  # Size of the kernel
+    r0 = int(0.4 * sizeVM)  # Radius shift from the center (8)
+    rho = 0.01 * sizeVM  # Scale coefficient to control arc length (0.1)
+    theta = np.pi * 3 / 2  # Angle to control the orientation of the arc
+    # thick = 0.0001 * sizeVM  #(0.5)  thickness of the arc
+    offsetpxs = int(0.35 * sizeVM)  # (6 for )
+    offset = (offsetpxs, offsetpxs)
+    num_pyrVM = 5
+    strideVM = 1
+    # Create Von Mises (VM) filters with specified parameters
+    # The angles are generated in radians, ranging from 0 to 2π in steps of π/4
+    thetas = np.arange(0, 2 * np.pi, np.pi / 4)
+
+    VMkernels = create_vm_filters(thetas, sizeVM, rho, r0, offset)
+
+    ###### start again from the creation of the filters
+
+    plot_VMkernels(VMkernels, thetas)
