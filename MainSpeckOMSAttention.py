74878e485abefaa94cb89d230ec7135c7dedaeb1
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2025-01-18 15:33:48 +0100

    code for the deployment

--- MainSpeckOMSAttention.py
+++ MainSpeckOMSAttention.py
@@ -1,8 +1,9 @@
+
 import cv2
 import numpy as np
 from functions.OMS_helpers import *
 from functions.attention_helpers import AttentionModule
-from functions.Speck_helpers import Specksetup, fetch_events
+from functions.Speck_helpers import Specksetup
 import matplotlib
 import time
 
@@ -49,7 +50,34 @@ class Config:
     }
 
 
+def compute_OMS(window_pos,window_neg):
+    # compute OMS maps
+    OMSpos = torch.tensor(window_pos, dtype=torch.float32).unsqueeze(0).to(config.DEVICE)
+    OMSneg = torch.tensor(window_neg, dtype=torch.float32).unsqueeze(0).to(config.DEVICE)
 
+    OMSpos_map, indexes_pos = egomotion(OMSpos, net_center, net_surround, config.DEVICE, config.MAX_Y, config.MAX_X,
+                                        config.OMS_PARAMS['threshold'])
+    OMSneg_map, indexes_neg = egomotion(OMSneg, net_center, net_surround, config.DEVICE, config.MAX_Y, config.MAX_X,
+                                        config.OMS_PARAMS['threshold'])
+
+    OMSpos_map = OMSpos_map.squeeze(0).squeeze(0).cpu().detach().numpy()
+    OMSneg_map = OMSneg_map.squeeze(0).squeeze(0).cpu().detach().numpy()
+
+    cv2.imshow('Events pos map', window_pos)
+    cv2.imshow('Events neg map', window_neg)
+    cv2.imshow('OMS pos map', OMSpos_map)
+    cv2.imshow('OMS neg map', OMSneg_map)
+    cv2.waitKey(1)
+    return OMSpos_map, OMSneg_map
+
+def compute_attention(OMSpos_map,OMSneg_map):
+    vSlice = np.expand_dims(np.stack((OMSpos_map, OMSneg_map)), 0)
+    with torch.no_grad():
+        saliency_map = net_attention(
+            torch.tensor(vSlice, dtype=torch.float32).to(config.DEVICE)
+        ).cpu().numpy()
+    cv2.imshow('Saliency map', saliency_map*255)
+    return saliency_map
 
 if __name__ == '__main__':
     config = Config()
@@ -61,39 +89,16 @@ if __name__ == '__main__':
     # Initialise Speck
     sink, window_pos, window_neg, numevs, events_lock = Specksetup(config.RESOLUTION, config.DROP_RATE)
 
-
     last_update_time = time.time()
     while True:
         current_time = time.time()
         with events_lock:
             if current_time - last_update_time > config.UPDATE_INTERVAL:
                 if numevs[0] > 0:
-                    OMSpos = torch.tensor(window_pos, dtype=torch.float32).unsqueeze(0).to(config.DEVICE)
-                    OMSneg = torch.tensor(window_neg, dtype=torch.float32).unsqueeze(0).to(config.DEVICE)
-
-                    OMSpos_map, indexes_pos = egomotion(OMSpos, net_center, net_surround, config.DEVICE, config.MAX_Y, config.MAX_X,
-                                                    config.OMS_PARAMS['threshold'])
-                    OMSneg_map, indexes_pos = egomotion(OMSneg, net_center, net_surround, config.DEVICE, config.MAX_Y,
-                                                    config.MAX_X,
-                                                    config.OMS_PARAMS['threshold'])
-
-                    OMSpos_map = OMSpos_map.squeeze(0).squeeze(0).cpu().detach().numpy()
-                    OMSneg_map = OMSneg_map.squeeze(0).squeeze(0).cpu().detach().numpy()
-                    vSlice = np.expand_dims(np.stack((OMSpos_map, OMSneg_map)), 0)
-
-                    with torch.no_grad():
-                        saliency_map = net_attention(
-                            torch.tensor(vSlice, dtype=torch.float32).to(config.DEVICE)
-                        ).cpu().numpy()
-
-                    cv2.imshow('Events pos map', window_pos)
-                    cv2.imshow('Events neg map', window_neg)
-                    cv2.imshow('OMS map', OMSpos_map)
-                    cv2.imshow('OMS map', OMSneg_map)
-                    cv2.imshow('Saliency map', cv2.applyColorMap(
-                            (saliency_map*255).astype(np.uint8), cv2.COLORMAP_JET))
-
-                    cv2.waitKey(1)
+                    # compute OMS
+                    OMSpos_map, OMSneg_map = compute_OMS(window_pos, window_neg)
+                    # compute attention
+                    saliency_map = compute_attention(OMSpos_map, OMSneg_map)
                     window_pos.fill(0)
                     window_neg.fill(0)
                 last_update_time = current_time
